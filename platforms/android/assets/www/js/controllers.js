02a781e118b38abdccd46f840e69301e74c3fc51
Xavier Gonzalez <xavier457@hotmail.com>
2016-02-01 16:06:37 -0430

    fixed

--- platforms/android/assets/www/js/controllers.js
+++ platforms/android/assets/www/js/controllers.js
@@ -22,7 +22,7 @@ angular.module('controllers', [])
       }).error(function(error, status, headers, config){
         $ionicPopup.alert({
           title: 'Error',
-          content: config + status + password + username
+          content: "tes" + config + status + password + username
         });
       });
     };
